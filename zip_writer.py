d0aa2c1f5862f011320b9b4e6e3c25f791427090
marianded <82287859+marianded@users.noreply.github.com>
2022-11-29 17:02:36 +0100

    Add files via upload

--- zip_writer.py
+++ zip_writer.py
@@ -19,7 +19,7 @@ values = []
 
 print(file_size)
 
-with h5py.File(f"{file_path}/{file_name}.h5", "r") as f_in:
+with h5py.File(f"{file_path}/{file_name}.h5", "r", rdcc_nbytes=5252880) as f_in:
     #image_data = f_in['Image'][:]
     #image_shape = np.shape(image_data)
     #print(image_shape)
@@ -54,8 +54,9 @@ def compression():
                 if key == "00":
                     for key2 in list(image_attributs.keys()):
                         dset2.attrs[key2] = image_attributs[key2]
-                    image_ref = f_zipped.create_dataset("Image", (1,), dtype=h5py.ref_dtype)
-                    image_ref[0] = dset2.ref
+                    f_zipped["Image"] = h5py.SoftLink("/pyramid/00")
+                    #image_ref = f_zipped.create_dataset("Image", (1,), dtype=h5py.ref_dtype)
+                    #image_ref[0] = dset2.ref
                 else:
                     dset2.attrs["scale"] = values[int(key)]
         end = t.monotonic_ns()
@@ -129,8 +130,9 @@ def gzip_compression():
     plt.title("Analyse der Kompressionsalgritmen")
     plt.show()
 
-def runtime():
-    run_times = []
+
+def runtime() -> np.ndarray:
+    read_times = []
     for zipper, chunk in itertools.product(zippers, chunks):
         begin = t.monotonic_ns()
         with h5py.File(f"{file_name}_{zipper}_{chunk}.h5", "r") as f_in:
@@ -144,30 +146,70 @@ def runtime():
                     image_shape = np.shape(data[key])
                 values.append(f_in[f"pyramid/{key}"].attrs["scale"])
         end = t.monotonic_ns()
-        run_times.append((end - begin) / 1000000000)
+        read_times.append((end - begin) / 1000000000)
         print(f"{zipper} extract time: {(end - begin) / 1000000000} s")
 
     # Plot
-    run_times = np.reshape(run_times, (len(zippers), len(chunks))).T
+    read_times = np.reshape(read_times, (len(zippers), len(chunks))).T
+    title = "Analyse der Kompressionsalgritmen"
+    xlabel = "Verlustfreie Kompressionsalgorithmen"
+    ylabel = "Leselaufzeit in s"
+    legend = ('Auto chunk', 'None chunk', '256x256x1', f'256x256x{image_shape[2]}')
+    # plot(read_times, title, zippers, legend, xlabel, ylabel)
+    return read_times
 
-    Labels = zippers
-    y_pos = np.arange(len(Labels))
-    plt.bar(y_pos + 0, run_times[0], width=0.2, color='navy', label='Auto chunk')
-    plt.bar(y_pos + 0.2, run_times[1], width=0.2, color='skyblue', label='None chunk')
-    plt.bar(y_pos + 0.4, run_times[2], width=0.2, color='darkcyan', label='256x256x1')
-    plt.bar(y_pos + 0.6, run_times[3], width=0.2, color='black', label=f'256x26x{image_shape[2]}')
 
-    plt.xticks(y_pos, Labels)
-    plt.legend(('Auto chunk', 'None chunk', '256x256x1', f'256x256x{image_shape[2]}'))
-    plt.xlabel('Verlustfreie Kompressionsalgorithmen')
-    plt.ylabel('Leselaufzeit in s')
-    plt.title("Analyse der Kompressionsalgritmen")
+def run_x_times(i: int):
+    read_times = []
+    for _ in range(i):
+        read_times.append(runtime())
+    print(read_times)
+    errors = np.array(0)
+
+    middle_times = np.average(read_times, axis=0)
+    print("middle", middle_times)
+    # read_times = np.rot90(read_times, 1, (2, 0))
+    errors1 = np.max(read_times, axis=0)
+    print("e1", errors1)
+    errors2 = np.min(read_times, axis=0)
+    print("e2", errors2)
+    errors = np.concatenate((np.max(read_times, axis=0), np.min(read_times, axis=0))).reshape((middle_times.shape[0], middle_times.shape[1], 2))
+    print("error", errors)
+
+    # Plot
+    title = "Analyse der Kompressionsalgritmen"
+    xlabel = "Verlustfreie Kompressionsalgorithmen"
+    ylabel = "Leselaufzeit in s"
+    legend = ('Auto chunk', 'None chunk', '256x256x1', f'256x256x{image_shape[2]}')
+    plot(middle_times, title, zippers, legend, xlabel, ylabel, errors)
+
+
+def plot(plot_data: np.ndarray, title: str, labels: np.ndarray, legend: tuple, xlabel: str, ylabel: str, error: np.ndarray=[]):
+    y_pos = np.arange(len(labels))
+    plt.bar(y_pos + 0, plot_data[0], width=0.2, color='navy', label='Auto chunk')
+    plt.bar(y_pos + 0.2, plot_data[1], width=0.2, color='skyblue', label='None chunk')
+    plt.bar(y_pos + 0.4, plot_data[2], width=0.2, color='darkcyan', label='256x256x1')
+    plt.bar(y_pos + 0.6, plot_data[3], width=0.2, color='black', label=f'256x26x{image_shape[2]}')
+
+    if len(error):
+        (_, caps, _) = plt.errorbar(
+            , plot_data, yerr=error, fmt='o', markersize=8, capsize=20)
+
+        for cap in caps:
+            cap.set_markeredgewidth(1)
+
+    plt.xticks(y_pos, labels)
+    plt.legend(legend)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
     plt.show()
 
 
 #compression()
-runtime()
+#runtime()
 #gzip_compression()
+run_x_times(5)
 
 # TODO SZIP Lizens
 # TODO Referenz von orginal Bild
