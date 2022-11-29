be6a546cbeddd8c809fb0bae9238cdeb2713abc5
marianded <82287859+marianded@users.noreply.github.com>
2022-11-29 12:59:08 +0100

    new

--- zip_writer.py
+++ zip_writer.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 file_name = "BrainEmptyLMP3D-01_00_s2208_PM_Complete_Raw_Tiles_Flat_v002"
 # file_name = "PE-2011-00015-H_00_s0199_PM_Complete_Raw_Tiles_Flat_v003"
 
-# file_path = "/data/PLI-Group/felix/data/hdf5-test"
-file_path = "E:\MD\Seminararbeit"
+file_path = "/data/PLI-Group/felix/data/hdf5-test"
+# file_path = "E:\MD\Seminararbeit"
 file_stats = os.stat(f"{file_path}/{file_name}.h5")
 file_size = file_stats.st_size
 data = {}
@@ -35,14 +35,14 @@ with h5py.File(f"{file_path}/{file_name}.h5", "r") as f_in:
 
 chunks = [None]
 chunks = [True, None, (256, 256, 1), (256, 256, image_shape[2])]
-zippers = ["lzf", "gzip"]
+zippers = ["szip", "lzf", "gzip"]
 
 
 def compression():
     compression_rates = []
     for zipper, chunk in itertools.product(zippers, chunks):
         begin = t.monotonic_ns()
-        with h5py.File(f"{file_path}/{file_name}_{zipper}_{chunk}.h5", "w") as f_zipped:
+        with h5py.File(f"{file_name}_{zipper}_{chunk}.h5", "w") as f_zipped:
             pyramid = f_zipped.create_group("pyramid")
             for key in pyramid_attributs:
                 pyramid.attrs[key] = pyramid_attributs[key]
@@ -59,11 +59,11 @@ def compression():
                 else:
                     dset2.attrs["scale"] = values[int(key)]
         end = t.monotonic_ns()
-        zipped_file_stats = os.stat(f"{file_path}/{file_name}_{zipper}_{chunk}.h5")
+        zipped_file_stats = os.stat(f"{file_name}_{zipper}_{chunk}.h5")
         zipped_file_size = zipped_file_stats.st_size
         compression_rate = (1 - zipped_file_size / file_size) * 100
         compression_rates.append(compression_rate)
-        print(f"{zipper} zip time {chunk}: {(end - begin) / 100000000}s")
+        print(f"{zipper} zip time {chunk}: {(end - begin) / 1000000000}s")
         print(f"Compression rate: {compression_rate}")
         print()
 
@@ -89,7 +89,7 @@ def gzip_compression():
     compression_rates = []
     for chunk, i in itertools.product(chunks, np.arange(10)):
         begin = t.monotonic_ns()
-        with h5py.File(f"{file_path}/{file_name}_gzip_{chunk}.h5", "w") as f_zipped:
+        with h5py.File(f"{file_name}_gzip_{chunk}.h5", "w") as f_zipped:
             pyramid = f_zipped.create_group("pyramid")
             for key in pyramid_attributs:
                 pyramid.attrs[key] = pyramid_attributs[key]
@@ -106,19 +106,34 @@ def gzip_compression():
                 else:
                     dset2.attrs["scale"] = values[int(key)]
         end = t.monotonic_ns()
-        zipped_file_stats = os.stat(f"{file_path}/{file_name}_gzip_{chunk}.h5")
+        zipped_file_stats = os.stat(f"{file_name}_gzip_{chunk}.h5")
         zipped_file_size = zipped_file_stats.st_size
         compression_rate = (1 - zipped_file_size / file_size) * 100
         compression_rates.append(compression_rate)
-        print(f"gzip zip time chunk {chunk}, compression_opts {i}: {(end - begin) / 100000000}s")
+        print(f"gzip zip time chunk {chunk}, compression_opts {i}: {(end - begin) / 1000000000}s")
         print(f"Compression rate: {compression_rate}")
         print()
+
+        # Plot
+    compression_rates = np.reshape(compression_rates, (len(chunks), 10)).T
+
+    Labels = chunks
+    y_pos = np.arange(len(Labels))
+    for i in np.arange(10):
+        plt.bar(y_pos + (i / 10.), compression_rates[i], width=0.1, color="", label='Auto chunk')
+
+    plt.xticks(y_pos, Labels)
+    plt.legend(('Auto chunk', 'None chunk', '256x256x1', f'256x256x{image_shape[2]}'))
+    plt.xlabel('gzip compression_opts')
+    plt.ylabel('Kompression in %')
+    plt.title("Analyse der Kompressionsalgritmen")
+    plt.show()
 
 def runtime():
     run_times = []
     for zipper, chunk in itertools.product(zippers, chunks):
         begin = t.monotonic_ns()
-        with h5py.File(f"{file_path}/{file_name}_{zipper}_{chunk}.h5", "r") as f_in:
+        with h5py.File(f"{file_name}_{zipper}_{chunk}.h5", "r") as f_in:
             for key in list(f_in["Image"].attrs.keys()):
                 image_attributs[key] = f_in["Image"].attrs[key]
             for key in list(f_in["pyramid"].attrs.keys()):
@@ -129,8 +144,8 @@ def runtime():
                     image_shape = np.shape(data[key])
                 values.append(f_in[f"pyramid/{key}"].attrs["scale"])
         end = t.monotonic_ns()
-        run_times.append((end - begin) / 100000000)
-        print(f"{zipper} extract time: {(end - begin) / 100000000} s")
+        run_times.append((end - begin) / 1000000000)
+        print(f"{zipper} extract time: {(end - begin) / 1000000000} s")
 
     # Plot
     run_times = np.reshape(run_times, (len(zippers), len(chunks))).T
@@ -151,8 +166,8 @@ def runtime():
 
 
 #compression()
-#runtime()
-gzip_compression()
+runtime()
+#gzip_compression()
 
 # TODO SZIP Lizens
 # TODO Referenz von orginal Bild
